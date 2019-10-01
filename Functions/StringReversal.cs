ae852f717a2652fce08d1516efbcda289ff1c76a
Patrick Mounts <mountspb@gmail.com>
2019-10-01 13:47:04 -0700

    added class for multiple string reversal functions

---
+++ Functions/StringReversal.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+/// <summary>
+/// given a string, reverse the string
+/// </summary>
+public static class ReverseString
+{
+    public static string ReverseStringWhile(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+        int Length = s.Length - 1;
+        while (Length >= 0)
+        {
+            sb.Append(s[Length]);
+            Length--;
+        }
+        return sb.ToString();
+    }
+    public static string ReverseStringLinqSimple(string s)
+    {
+        String sr = new string(s.Reverse().ToArray());
+        return sr.ToString();
+    }
+
+    public static string ReverseStringLinq(string s)
+    {
+        string sr = s.Select(ch => ch.ToString())
+                     .Aggregate<string>((xs, x) => x + xs);
+        return sr;
+    }
+
+    public static string ReverseStringLinqPerformance(string s)
+    {
+        string sr = s.Aggregate(new StringBuilder(), (x, y) => x.Insert(0, y)).ToString();
+        return sr;
+    }
+    public static string ReverseStringFor(string s)
+    {
+        int index = s.Length - 1;
+        string sr = "";
+
+        for (int i = index; index >= 0; index--)
+        {
+            sr = sr + (s.Substring(index, 1));
+        }
+        return sr;
+    }
+}
